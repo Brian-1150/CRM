eeab3cbd601d7dea4f9a94bdd4bac1b1ec956e4c
bpcam <bpcampassi@gmail.com>
2021-04-13 19:59:10 -0400

    Restructure of tables finished for now

--- CRM.Models/Invoice/InvoiceCreate.cs
+++ CRM.Models/Invoice/InvoiceCreate.cs
@@ -1,22 +1,23 @@
-﻿using System;
+﻿using CRM.Data;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CRM.Models.Invoice
 {
-  public  class InvoiceCreate
+    public class InvoiceCreate
     {
-        
-        public int InvoiceID { get; set; }
-                
+
+
         public int CustomerID { get; set; }
-      
 
+
+        [Display(Name = "Choose the customer")]
+
+        public virtual ICollection<CustomerListItem> ListOfCustomers { get; set; }
         public virtual ICollection<Job> Jobs { get; set; }
-        public double InvoiceAmount { get; set; }
-
-        public bool Paid { get; set; }
     }
 }
